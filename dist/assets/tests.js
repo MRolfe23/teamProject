ddf1c981b99e3ea53dd5fa3886adac92cde872ad
MRolfe23 <31248765+MRolfe23@users.noreply.github.com>
2017-12-08 09:51:51 -0600

    Add files via upload
    getting ember on git hub part 1

---
+++ dist/assets/tests.js
@@ -0,0 +1,142 @@
+'use strict';
+
+define('ember-quickstart/tests/app.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | app');
+
+  QUnit.test('app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('resolver.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'resolver.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('router.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass ESLint\n\n');
+  });
+});
+define('ember-quickstart/tests/helpers/destroy-app', ['exports'], function (exports) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = destroyApp;
+  var run = Ember.run;
+  function destroyApp(application) {
+    run(application, 'destroy');
+  }
+});
+define('ember-quickstart/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'ember-quickstart/tests/helpers/start-app', 'ember-quickstart/tests/helpers/destroy-app'], function (exports, _qunit, _startApp, _destroyApp) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  exports.default = function (name) {
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+    (0, _qunit.module)(name, {
+      beforeEach: function beforeEach() {
+        this.application = (0, _startApp.default)();
+
+        if (options.beforeEach) {
+          return options.beforeEach.apply(this, arguments);
+        }
+      },
+      afterEach: function afterEach() {
+        var _this = this;
+
+        var afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+        return resolve(afterEach).then(function () {
+          return (0, _destroyApp.default)(_this.application);
+        });
+      }
+    });
+  };
+
+  var resolve = Ember.RSVP.resolve;
+});
+define('ember-quickstart/tests/helpers/resolver', ['exports', 'ember-quickstart/resolver', 'ember-quickstart/config/environment'], function (exports, _resolver, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+
+  var resolver = _resolver.default.create();
+
+  resolver.namespace = {
+    modulePrefix: _environment.default.modulePrefix,
+    podModulePrefix: _environment.default.podModulePrefix
+  };
+
+  exports.default = resolver;
+});
+define('ember-quickstart/tests/helpers/start-app', ['exports', 'ember-quickstart/app', 'ember-quickstart/config/environment'], function (exports, _app, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = startApp;
+  var merge = Ember.merge;
+  var run = Ember.run;
+  function startApp(attrs) {
+    var attributes = merge({}, _environment.default.APP);
+    attributes = merge(attributes, attrs); // use defaults, but you can override;
+
+    return run(function () {
+      var application = _app.default.create(attributes);
+      application.setupForTesting();
+      application.injectTestHelpers();
+      return application;
+    });
+  }
+});
+define('ember-quickstart/tests/test-helper', ['ember-quickstart/tests/helpers/resolver', 'ember-qunit', 'ember-cli-qunit'], function (_resolver, _emberQunit, _emberCliQunit) {
+  'use strict';
+
+  (0, _emberQunit.setResolver)(_resolver.default);
+  (0, _emberCliQunit.start)();
+});
+define('ember-quickstart/tests/tests.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | tests');
+
+  QUnit.test('helpers/destroy-app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/destroy-app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/module-for-acceptance.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/resolver.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/resolver.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/start-app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/start-app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('test-helper.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass ESLint\n\n');
+  });
+});
+require('ember-quickstart/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
